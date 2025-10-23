1be7a0253991e4afff57e5a54a64aa34f4fdaebf
nonashvili <nonashvilisaba12@gmail.com>
2025-10-23 23:04:46 +0400

    commit

---
+++ day08/classwork/2cw.py
@@ -0,0 +1,9 @@
+mineral_water=input("enter mineral water:")
+
+
+if mineral_water=="likani":
+    print("wamovigot likani")
+elif mineral_water=="borjomi":
+    print("wamovigot borjomi")
+else:
+    print("wamovigot nabeglavi")
---
+++ day08/classwork/cw.py
@@ -0,0 +1,8 @@
+number=int(input("enter number:"))
+
+
+
+if number>50:
+   print("you won")
+else:
+   print("you lose")
