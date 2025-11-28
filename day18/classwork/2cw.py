5166e00c1deb3a1598cbd7383daa6f492fef528d
nonashvili <nonashvilisaba12@gmail.com>
2025-11-29 01:23:32 +0400

    commit

---
+++ day18/classwork/2cw.py
@@ -0,0 +1,16 @@
+#list siashi shegvidzlia ramdeni elementic minda imdeni elementi da ra monacemta elementic minda (integer,string,float,boolean)
+
+
+
+#wesi --> vwert cvladis saxels+ ttolobis nishani +kvadrtatuli frchxilebi
+
+#wsi2 --> siashi myofi elementebi ertmanetisgan gamoiyofa mdzimit
+
+
+lasha=[1,2,3,4]
+
+names=["goga","giorgi",20,True,"20.5",["lasha",100,"10.6"], False]
+
+print(names)
+
+
---
+++ day18/classwork/cw.py
@@ -0,0 +1,14 @@
+age=13
+
+name="durmishxani"
+
+if age >= 18:
+    if name=="durmishxani":
+        print("sen srulwlovani xar da aseve sheni saxeli ari durmishxani")
+    else:
+        print("shen xar srulwolovaNI MAGRAM SHENI SAXELI AR ARIS DURISHXANI")
+elif age<18:
+    if name=="durmishxani":
+        print("patara xar magram sheni saxeli ariss durmishxaani")
+    else:
+        print("patara xar magram sheni saxeli ar aris durmishxani")
